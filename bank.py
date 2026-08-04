c725d0faf6cd7a58589f21c250d7aeb1be402bf0
Hajar <code.with.hajar@gmail.com>
2026-08-04 12:02:10 +0100

    Implement Python Bank System v1

--- bank.py
+++ bank.py
@@ -12,20 +12,22 @@ while choice != 4:
     choice = int(input("enter your number "))
     if choice == 4:
         print(f"Thank you for using Python Bank.")
-        exit
+        break
     elif choice == 1:
         deposit = float(input("Enter deposit amount: "))
-        if deposit < 0:
+        if deposit <= 0:
             print("Invalid amount.")
         else:
-            balance = balance + deposit
+            balance += deposit
             print(f"The new balance is: {balance}")
     elif choice == 2:
         withdraw = float(input("Enter withdrawal amount: "))
-        if withdraw < 0 or withdraw > balance:
-            print(f"Insufficient funds.")
+        if withdraw <= 0:
+            print("Invalid amount.")
+        elif withdraw > balance:
+            print("Insufficient funds.")
         else:
-            balance = balance - withdraw
+            balance -= withdraw
             print(f"The new balance is: {balance}")
     elif choice == 3:
         print(f"Current balance: {balance}")
