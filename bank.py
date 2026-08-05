21dddd7f7d5c8501a70adc255af31227358ab908
Hajar <code.with.hajar@gmail.com>
2026-08-05 11:51:41 +0100

    bank system using functions

--- bank.py
+++ bank.py
@@ -1,6 +1,4 @@
-choice = 1
-balance = 0.0
-while choice != 4:
+def show_menu():
     print("========== PYTHON BANK ==========")
     print("")
     print("1. Deposit")
@@ -9,27 +7,46 @@ while choice != 4:
     print("4. Exit")
     print("")
     print("=================================")
-    choice = int(input("enter your number "))
-    if choice == 4:
-        print(f"Thank you for using Python Bank.")
-        break
-    elif choice == 1:
-        deposit = float(input("Enter deposit amount: "))
-        if deposit <= 0:
-            print("Invalid amount.")
-        else:
-            balance += deposit
-            print(f"The new balance is: {balance}")
-    elif choice == 2:
-        withdraw = float(input("Enter withdrawal amount: "))
-        if withdraw <= 0:
-            print("Invalid amount.")
-        elif withdraw > balance:
-            print("Insufficient funds.")
-        else:
-            balance -= withdraw
-            print(f"The new balance is: {balance}")
-    elif choice == 3:
-        print(f"Current balance: {balance}")
+
+def deposit(balance):
+    amount = float(input("Enter deposit amount: "))
+    if deposit <= 0:
+        print("Invalid amount.")
     else:
-        print("Invalid menu option.")
+        balance += amount
+    return balance
+
+def withdraw(balance):
+    amount = float(input("Enter withdrawal amount: "))
+    if amount <= 0:
+        print("Invalid amount.")
+    elif amount > balance:
+        print("Insufficient funds.")
+    else:
+        balance -= amount
+    return balance
+
+def check_balance(balance):
+    print(f"Current balance: {balance}")
+
+def main():
+    balance = 0.0
+
+    while True:
+        show_menu()
+        choice = int(input("enter your choice: "))
+        if choice == 1:
+            balance = deposit(balance)
+            print(f"The new balance is: {balance}")
+        elif choice == 2:
+            balance = withdraw(balance)
+            print(f"The new balance is: {balance}")
+        elif choice == 3:
+            check_balance(balance)
+        elif choice == 4:
+            print(f"Thank you for using Python Bank.")
+            break
+        else:
+            print("Invalid menu option.")
+
+main()
