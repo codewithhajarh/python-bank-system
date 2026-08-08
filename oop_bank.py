955dbe15f3009d7b55f71642283deb92e170a724
Hajar <code.with.hajar@gmail.com>
2026-08-08 21:38:40 +0100

    Add transaction history and transfers

--- oop_bank.py
+++ oop_bank.py
@@ -2,11 +2,13 @@ class Account:
     def __init__(self, owner, balance):
         self.owner = owner
         self.balance = balance
+        self.transactions = []
     def deposit(self, amount):
         if amount <= 0:
             print("Invalid amount.")
         else:
             self.balance += amount
+            self.transactions.append(f"Deposit: {amount}")
     def withdraw(self, amount):
         if amount <= 0:
             print("Invalid amount.")
@@ -14,8 +16,32 @@ class Account:
             print("Insufficient funds.")
         else:
             self.balance -= amount
+            self.transactions.append(f"Withdraw: -{amount}")
+    def transfer(self, other_account, amount):
+        if other_account.owner == self.owner :
+            print("Invalid account")
+        elif amount <= 0:
+            print("Invalid amount.")
+        elif amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount
+            other_account.balance += amount
+            self.transactions.append(f"Transfer to {other_account.owner}: -{amount}")
+            other_account.transactions.append(f"Transfer from {self.owner}: +{amount}")
+            print("Transfer is successful!")
     def check_balance(self):
         print(f"Current balance: {self.balance}")
+    def show_transactions(self):
+        print("========== TRANSACTIONS ==========")
+        print("")
+        if len(self.transactions) > 0:
+            for i in range(len(self.transactions)):
+                print(f"{self.transactions[i]}\n")
+        else:
+            print("No transations yet!")
+        print("")
+        print("==================================")
 
 class Bank:
     def __init__(self, accounts, current_account):
@@ -74,9 +100,11 @@ def menu_login(current_account):
     print("")
     print("1. Deposit")
     print("2. Withdraw")
-    print("3. Check balance")
-    print("4. Logout")
-    print("5. Exit")
+    print("3. Transfer")
+    print("4. Check balance")
+    print("5. Transactions")
+    print("6. Logout")
+    print("7. Exit")
     print("")
     print("=================================")
 
@@ -108,27 +136,49 @@ def main():
             else :
                 print("Invalid menu option.")
         else :
-            menu_login(current_account)
+            menu_login(bank.current_account)
             try:
                 choice = int(input("Enter your choice: "))
             except ValueError:
                 print("Invalid menu option.")
                 continue
             if choice == 1:
-                amount = float(input("Enter deposit amount: "))
-                current_account.deposit(amount)
-                print(f"The new balance is: {current_account.balance}")
+                try:
+                    amount = float(input("Enter deposit amount: "))
+                except ValueError:
+                    print("Invalid amount.")
+                    continue
+                bank.current_account.deposit(amount)
+                print(f"The new balance is: {bank.current_account.balance}")
                 save_accounts(accounts)
             elif choice == 2:
-                amount = float(input("Enter withdrawal amount: "))
-                current_account.withdraw(amount)
-                print(f"The new balance is: {current_account.balance}")
+                try:
+                    amount = float(input("Enter withdrawal amount: "))
+                except ValueError:
+                    print("Invalid amount.")
+                    continue
+                bank.current_account.withdraw(amount)
+                print(f"The new balance is: {bank.current_account.balance}")
                 save_accounts(accounts)
             elif choice == 3:
-                current_account.check_balance()
+                name = input("Enter the owner of account you want to transfer to: ")
+                try:
+                    amount = float(input("Enter the amount you want to transfer: "))
+                except ValueError:
+                    print("Invalid amount.")
+                    continue
+                if name in bank.accounts:
+                    other_account = bank.accounts[name]
+                    bank.current_account.transfer(other_account, amount)
+                else: 
+                    print("Invalid account!")
             elif choice == 4:
-                bank.logout()
+                current_account.check_balance()
             elif choice == 5:
+                bank.current_account.show_transactions()
+            elif choice == 6:
+                bank.logout()
+            elif choice == 7:
                 print(f"Thank you for using Python Bank.")
                 break
             else:
